d5ecfa4062e17b0a9c2a9918be9482d7d0fdd251
Kazagan <Steffen.Paige@outlook.com>
2021-11-23 08:56:38 -0600

    parsing first 10 lines

--- problems/euler13.c
+++ problems/euler13.c
@@ -5,6 +5,7 @@
 
 #define MAX_LINE 500
 
+
 void die(char *message) {
     if(errno) {
         perror(message);
@@ -13,6 +14,7 @@ void die(char *message) {
     }
     exit(1);
 }
+
 
 int line_count(FILE *fp) {
     char buff[MAX_LINE];
@@ -44,10 +46,6 @@ char** get_lines(FILE *fp, int lines) {
     return list;
 }
 
-long last_ten(long long n) {
-
-}
-
 int main(int argc, char *argv[]) {
     if(argc < 2)
         die("USAGE: euler13 <fileName>\n File should include at least one 10+digit number, and will print the first 10 digits of the sum of the numbers. \n Each number should be on a new line.");
@@ -65,9 +63,32 @@ int main(int argc, char *argv[]) {
         die("failed to get line count");
 
     char** lines = get_lines(fptr, lines_count);
+    long sum = 0;
+    for(int i = 0; i < lines_count; i ++ ){
+        char *line = lines[i];
+        int size = strlen(line);
+        char *x = malloc(11);
+        strncpy(x, line+(size-10), 11);
+        x[11] = '\0';
+        sum += atol(x);
 
-    long long x = atoll(lines[0]);
-    printf("%lld\n", x);
+        printf("%ld\n", atol(x));
+        free(x);
+    }
+    printf("%ld\n", sum);
+    int n = 10;
+    long x = 0;
+    while(n != 0) {
+        int y = sum % 10;
+        sum = sum / 10;
+        x += y;
+        x *= 10;
+        n--;
+        printf("%ld %ld\n", x, sum);
+    }
+    printf("%ld\n", x);
+
+
     fclose(fptr);
     return 0;
 }
@@ -77,4 +98,9 @@ int main(int argc, char *argv[]) {
  *  That was a terrible idea....
  *  but it's working now, and now I'm looking at starting with a single number, and returning the first 10 digits, before
  *  moving onto sum them up, and do the same.
- */
+ *
+ *  So Working with a number...
+ *  12345
+ *  if we want five, we can divide by 10, and take the remainder. or mod 10
+ *  135,740,250
+*/
